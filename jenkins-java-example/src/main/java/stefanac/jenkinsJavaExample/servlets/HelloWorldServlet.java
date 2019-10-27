5c0b8289b6cd4b0c21cad7d1a0af1257db860e37
maximilianstefanac <maximilian.stefanac@googlemail.com>
2019-10-27 16:45:58 +0100

    initial setup of maven web project

---
+++ jenkins-java-example/src/main/java/stefanac/jenkinsJavaExample/servlets/HelloWorldServlet.java
@@ -0,0 +1,49 @@
+package stefanac.jenkinsJavaExample.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+public class HelloWorldServlet extends HttpServlet {
+	
+       
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6345011198903280009L;
+
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter responseWriter = response.getWriter();
+		responseWriter.write("Served at:");
+		responseWriter.write(request.getContextPath());
+		responseWriter.flush();
+		responseWriter.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
